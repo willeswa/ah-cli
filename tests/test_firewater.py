2f8e9c3d43b5c94fe4978d8ef2637a391650b8f6
willeswa <gwiliez@gmail.com>
2019-06-19 09:54:46 +0300

    add ability to export to json
    [Delivers #166785311]

--- tests/test_firewater.py
+++ tests/test_firewater.py
@@ -1,19 +1,38 @@
-
-from firewater import view, get_resource_url
+import os
+from firewater import view, list, export_to_json
 
 
 def test_get_specific_artcle(runner):
     """can get a specific article
 
     """
-    response = runner.invoke(view)
+    response = runner.invoke(view, "new_ah")
     assert 'Success' in response.output
 
 
-def test_get_non_existent_article(runner):
+def test_get_list_of_article(runner):
     """Returns correct error when resource is unavailable
 
     """
-    response = runner.invoke(view, "--slug clesome")
+    response = runner.invoke(list)
+    assert 'Success' in response.output
+
+
+def test_non_existant_article(runner):
+    """correct response for no artucle
+    """
+    response = runner.invoke(view, "new")
     assert 'Not Found' in response.output
 
+
+def test_possible_to_export_file(runner):
+    """tests that a file can be created
+    """
+
+    export_to_json(
+        {
+            "some": "data about nothing"
+        },
+        'test_file')
+    assert os.path.isfile('test_file.json')
+    os.remove('test_file.json')
